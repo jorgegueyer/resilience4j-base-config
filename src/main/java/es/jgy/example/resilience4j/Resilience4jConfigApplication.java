759f93e936cbc9a86142bbef637a0b4c7d24e9ec
Jorge Guerra Yerpes <jorgegueyer@gmail.com>
2021-05-09 13:41:27 +0200

    First commit
    Base files project.

---
+++ src/main/java/es/jgy/example/resilience4j/Resilience4jConfigApplication.java
@@ -0,0 +1,13 @@
+package es.jgy.example.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jConfigApplication.class, args);
+	}
+
+}
